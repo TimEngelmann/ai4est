9aafa5df5c8e6648833d049bb9c1ea64035c8909
Jan Philipp Bohl <jpbohl@protonmail.com>
2022-11-03 16:10:57 +0100

    Finished processing file

--- pipeline/parts/data_split.py
+++ pipeline/parts/data_split.py
@@ -6,27 +6,8 @@ import torch
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def create_patches_dataframe(path):
-    files = list(Path(path).glob('**/*.npz'))
-    df = list()
 
-    for i in range(len(files)):
-        print(i)
-        data = np.load(str(files[i]))
-        path_to_img=files[i]
-        carbon = data["carbon"]
-        site = data["site"].item()
-        rotation = data["rotation"].item()
-        idx = 0
-        for key in list(data.keys()):
-            if (key != "carbon") & (key != "site") & (key != "rotation"):
-                #image = data[key][0:3, :, :]
-                name= key
-                new = {"path": path_to_img, "name": name, "carbon": carbon[idx], "site": site, "rotation": rotation}
-                df.append(new)
-                idx += 1
-    return pd.DataFrame(df)
-def create_split_dataframe(path: str, method:str, splits):
+def create_split_dataframe(path: str, method:str, data:pd.DataFrame, splits):
     """
     path: to the images
     method: "across_sites" or "by_site"
@@ -46,8 +27,8 @@ def create_split_dataframe(path: str, method:str, splits):
         ptrain, pval, ptest=splits
         assert(ptrain+pval+ptest==1)
         for i in range(len(sites)):
-            patches_site=patches.loc[patches['site']==sites[i]]
-            train_site, test_val_site = train_test_split(patches_site, train_size=ptrain)
+            patches_site=data.loc[patches['site']==sites[i]]
+            train_site, test_val_site = train_test_split(data, train_size=ptrain)
             val_site, test_site= train_test_split(test_val_site, train_size=pval/(pval+ptest))
             train_dataset= pd.concat([train_dataset, train_site])
             val_dataset = pd.concat([val_dataset, val_site])
@@ -60,18 +41,21 @@ def create_split_dataframe(path: str, method:str, splits):
         np.random.shuffle(idx)
         print(idx)
         for i in idx[0:ntrain]:
-            patches_site = patches.loc[patches['site'] == sites[i]]
+            patches_site = data.loc[patches['site'] == sites[i]]
             train_dataset = pd.concat([train_dataset, patches_site])
         for i in idx[ntrain:(ntrain+nval)]:
-            patches_site = patches.loc[patches['site'] == sites[i]]
+            patches_site = data.loc[patches['site'] == sites[i]]
             val_dataset = pd.concat([val_dataset, patches_site])
         for i in idx[(ntrain+nval):6]:
-            patches_site = patches.loc[patches['site'] == sites[i]]
+            patches_site = data.loc[patches['site'] == sites[i]]
             test_dataset = pd.concat([test_dataset, patches_site])
     train_dataset = train_dataset.reset_index(drop=True)
     val_dataset = val_dataset.reset_index(drop=True)
     test_dataset = test_dataset.reset_index(drop=True)
+    
     return train_dataset, val_dataset, test_dataset
+
+
 class PatchesDataSet(Dataset):
     def __init__(self, path, df, transform=None):
         """
@@ -82,34 +66,48 @@ class PatchesDataSet(Dataset):
         """
         self.path = path
         # "/Users/victoriabarenne/Documents/ai4good2/dataset/"
-        self.files = list(Path(self.path).glob('**/*.npz'))
         self.df = df
-        self.transform= transform
+        self.transform = transform
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, item):
-        name=self.df["name"][item]
-        path=self.df["path"][item]
-        carbon = self.df["carbon"][item]
+        """
+        Loading samples based on the information contained
+        in self.df.
+        """
+        path = self.path + self.df["path"][item]
         site = self.df["site"][item]
         rotation = self.df["rotation"][item]
-        image= np.load(path)[name][0:3, :, :]
-        image = np.moveaxis(image, 0, -1)
+        i, j = self.df["site index"][item]
+        
+        image = torch.load(path)[:, i, j, ...]
+        carbon = self.df["carbon"][item]
+
+        assert image.shape[0] == 3
+        assert image.ax
+
+
         if self.transform is not None:
             image = self.transform(image)
+
         return image, carbon, site, rotation
-def train_val_test_dataset(path: str, method:str, splits, transform=ToTensor()):
+
+
+def train_val_test_dataset(path: str, method:str, splits):
     train, val, test= create_split_dataframe(path, method, splits)
-    train_dataset= PatchesDataSet(path, train, transform)
-    val_dataset = PatchesDataSet(path, val, transform)
-    test_dataset = PatchesDataSet(path, test, transform)
+    train_dataset= PatchesDataSet(path, train)
+    val_dataset = PatchesDataSet(path, val)
+    test_dataset = PatchesDataSet(path, test)
+    
     return train_dataset, val_dataset, test_dataset
-def train_val_test_dataloader(path:str, method:str, splits, batch_size, transform=ToTensor()):
-    train_dataset, val_dataset, test_dataset= train_val_test_dataset(path, method, splits, transform)
+
+
+def train_val_test_dataloader(path:str, method:str, splits, batch_size):
+    train_dataset, val_dataset, test_dataset= train_val_test_dataset(path, method, splits)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    
     return train_loader, val_loader, test_loader
-
